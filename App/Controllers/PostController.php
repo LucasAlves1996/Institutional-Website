378e318d622661223db8ab108e9a30155a7ca794
Lucas Costa Alves <lucas.alves263@gmail.com>
2019-08-20 13:21:16 -0300

    Correção em erro de sintaxe.

--- App/Controllers/PostController.php
+++ App/Controllers/PostController.php
@@ -20,11 +20,7 @@ class PostController extends Action
         header('Location: /blog');
     }
 
-<<<<<<< HEAD
     public static function read()
-=======
-    public function read()
->>>>>>> 9f33f8a7fcc17a516ecbcf55b09885b0815bb67e
     {
         $posts = Container::getModel('Post');
 
